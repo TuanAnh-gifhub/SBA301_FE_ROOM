9b689192df48e3ba1ff619a82a8657c474e541f0
Tran Le Tuan Anh (K17 HCM) <anhtltse173444@fpt.edu.vn>
2026-03-10 00:24:15 +0700

    update payment method by wallet or Payos

--- frontend/src/pages/Customer/Booking/PaymentSummary.tsx
+++ frontend/src/pages/Customer/Booking/PaymentSummary.tsx
@@ -83,7 +83,7 @@ export default function PaymentSummary({ intent }: any) {
                 : ""
             }`}
           >
-            Ví nội bộ
+            Ví cá nhân
           </button>
 
           <button
@@ -92,7 +92,7 @@ export default function PaymentSummary({ intent }: any) {
               paymentMethod === "VN_PAY" ? "border-teal-500 bg-teal-50" : ""
             }`}
           >
-            PayOS trực tiếp
+            PayOS 
           </button>
         </div>
       </div>
